f1cc08ae3f1a2c3fa31736a75159f48232aa10ba
TRUST MARVINZ <trustmarvinz48@gmail.com>
2025-06-08 19:14:27 +0300

    Move static files to client folder and update Express to serve them

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello from TRUST MARVINZ Node.js website!</h1>');
+// Serve static files from the 'client' folder
+app.use(express.static(path.join(__dirname, 'client')));
+
+// Optional: fallback for any other routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'index.html'));
 });
 
 app.listen(port, () => {
