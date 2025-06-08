e6a64cf416159e88ed1bf614ca72721cc8d6c5cc
TRUST MARVINZ <trustmarvinz48@gmail.com>
2025-06-08 19:44:29 +0300

    Initial commit: Node.js app ready for deploy

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
+// Route for root URL - sends "Hello, world!"
 app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
+// Serve static files from 'client' directory for other requests
+app.use(express.static(path.join(__dirname, 'client')));
+
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
